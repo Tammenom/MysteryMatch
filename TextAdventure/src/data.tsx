733f9dbe6531211ab774af04575c3e5f72c27f38
Wokou <33290853+Tammenom@users.noreply.github.com>
2023-08-29 20:14:32 +0200

    player umstrukturierung start

--- TextAdventure/src/data.tsx
+++ TextAdventure/src/data.tsx
@@ -1,6 +1,24 @@
+interface Player {
+  name: string;
+  playerID: number;
+  wonGames: number;
+  isCurrentPlayer: boolean;
+  score: number;
+}
+
+export const playerArray: Player[] = [];
+
 export const namesArray: string[] = [];
 
 export const updateNamesArray = (index: number) => {
   namesArray.splice(index, 1);
   console.log("Index given: " + index + " Names array in data: " + namesArray);
+};
+
+export const addDefaultPlayer = (index: number) => {
+  playerArray.splice(index, 1);
+};
+
+export const deletePlayer = (index: number) => {
+  playerArray.splice(index, 1);
 };
